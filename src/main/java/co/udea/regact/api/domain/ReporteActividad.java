52ca1cb09bf8cc0af76a19255a80f9d0f864d297
Orvie Salgado E <orvie.salgado@ibm.com>
2018-05-30 09:56:50 -0500

    ajuste columna identity serial

--- src/main/java/co/udea/regact/api/domain/ReporteActividad.java
+++ src/main/java/co/udea/regact/api/domain/ReporteActividad.java
@@ -48,7 +48,7 @@ public class ReporteActividad implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="rep_id")
 	private Long repId;
 	
