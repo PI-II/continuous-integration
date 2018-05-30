55a93dd263be03b1d8779ffaf1da44af0a3a5b8f
Orvie Salgado E <orvie.salgado@ibm.com>
2018-05-29 22:23:31 -0500

    Ajustes despliegue como war

--- src/main/java/co/udea/regact/api/RegistroActividadesApiApplication.java
+++ src/main/java/co/udea/regact/api/RegistroActividadesApiApplication.java
@@ -1,11 +1,19 @@
 package co.udea.regact.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class RegistroActividadesApiApplication {	
+public class RegistroActividadesApiApplication extends SpringBootServletInitializer {	
 	public static void main(String[] args) {
 		SpringApplication.run(RegistroActividadesApiApplication.class, args);
 	}
+	
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	return application.sources(appClass);
+    }
+    
+    private static Class<RegistroActividadesApiApplication> appClass = RegistroActividadesApiApplication.class;
 }
