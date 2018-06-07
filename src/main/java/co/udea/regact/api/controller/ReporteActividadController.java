f9bee493813646b48f3045bc4a7ece297f1a7d48
Orvie Salgado E <orvie.salgado@ibm.com>
2018-06-06 21:17:02 -0500

    Mock para consultar registro de actividades por id de grupo

--- src/main/java/co/udea/regact/api/controller/ReporteActividadController.java
+++ src/main/java/co/udea/regact/api/controller/ReporteActividadController.java
@@ -1,5 +1,6 @@
 package co.udea.regact.api.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -33,8 +34,44 @@ public class ReporteActividadController {
 
 		registroActividadFacade.registrarActividad(registroActividad);
 	}
+	
+	@GetMapping("getregactbygrupo")
+	public List<ReporteActividadDto> getReporteActividadesByGrupoSemestreActivo(int idGrupo) {
+		
+		List<ReporteActividadDto> regs = new ArrayList<>();
 
-	@GetMapping("consultarsemestreactivo")
+		ReporteActividadDto dto = new ReporteActividadDto();
+		dto.setFechaReporte(new Date());
+		dto.setHoras(5);
+		dto.setIdActividad(2);
+		dto.setIdDocente(1);
+		dto.setIdGrupo(1);
+		dto.setIdSemestre(1);
+		dto.setNombreActividad("Laboraorio");
+		dto.setNombreGrupo("G-001");
+		dto.setObservaciones("un mock de prueba");
+		dto.setSemestre("2018-1");
+
+		regs.add(dto);
+		dto = new ReporteActividadDto();
+
+		dto.setFechaReporte(new Date());
+		dto.setHoras(5);
+		dto.setIdActividad(2);
+		dto.setIdDocente(1);
+		dto.setIdGrupo(1);
+		dto.setIdSemestre(1);
+		dto.setNombreActividad("TALLER");
+		dto.setNombreGrupo("G-001");
+		dto.setObservaciones("un mock de prueba 222");
+		dto.setSemestre("2018-1");
+
+		regs.add(dto);
+		
+		return regs;
+	}
+
+	@GetMapping("getregactbydocente")
 	public List<ReporteActividadDto> getReporteActividadesByDocenteSemestreActivo(int idDocente) {
 		
 //		Set<ReporteActividadDto> regs = new HashSet<>();
